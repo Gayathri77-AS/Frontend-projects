08fd6714c058bbefd2996b73427592cd4401afa2
Gayathri A S <gayathrisaravanan191@gmail.com>
2023-07-30 11:40:01 +0530

    Update script.js
    updated

--- web dev/frontend/today I learnt/script.js
+++ web dev/frontend/today I learnt/script.js
@@ -61,7 +61,6 @@ async function loadfacts(){
   const res = await fetch("https://eocpgzilbhighxgvnyrm.supabase.co/rest/v1/fact",{
     headers:{
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvY3BnemlsYmhpZ2h4Z3ZueXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk0ODU1NjIsImV4cCI6MjAwNTA2MTU2Mn0.X-2IXMs2NXc4vdQmlF3g3_Czc-kCxS6OiEhHW2C3qYY",
-      authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvY3BnemlsYmhpZ2h4Z3ZueXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk0ODU1NjIsImV4cCI6MjAwNTA2MTU2Mn0.X-2IXMs2NXc4vdQmlF3g3_Czc-kCxS6OiEhHW2C3qYY",
     },
   });
   const data = await res.json();
