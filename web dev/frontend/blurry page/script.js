8f7ab1b29199dce2972625b7eee777ea4d05ee69
gayu <gayathrisaravanan191@gmail.com>
2023-08-06 13:08:35 +0530

    cosmetic issue

--- web dev/frontend/blurry page/script.js
+++ web dev/frontend/blurry page/script.js
@@ -1,8 +1,6 @@
 const loadText = document.querySelector(".leading-text")
 const bg = document.querySelector(".bg")
-
 let load = 0
-
 let int = setInterval(blurring, 30)
 
 function blurring(){
@@ -11,10 +9,9 @@ function blurring(){
     clearInterval(int)
     }
 
- loadText.innerText = `${load}%`
- loadText.style.opacity = scale (load, 0, 100, 1, 0)
- bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-
+loadText.innerText = `${load}%`
+loadText.style.opacity = scale (load, 0, 100, 1, 0)
+bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
