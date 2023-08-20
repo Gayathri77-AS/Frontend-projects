98b6857afc8aa2ff22087fa05b98ec2321727834
gayu <gayathrisaravanan191@gmail.com>
2023-08-20 19:44:17 +0530

    modifications

--- today_i_learnt/today-i-learned/src/App.js
+++ today_i_learnt/today-i-learned/src/App.js
@@ -1,38 +1,174 @@
+import { useState } from "react";
 import "./style.css";
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:  "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+function Counter(){
+  const [count,setCount] = useState(0)
+
+  return (<div>
+    <span style={{fontSize:"40px"}}>{count} </span>
+    <button className="btn btn-large" onClick={()=>setCount((c)=>c+1)}>
+      +1
+      </button>
+  </div>
+  );
+}
 
 function App(){
+  const[showForm, setShowForm] = useState(false);
   return (
     <>
     {/* Header */}
-    <header className="header">
-      <div className="logo">
-      <img
-      src="logo512.png"
-      height="68"
-      width="68"
-      alt="Today I Learned Logo"
-    />
-    <h1>Today I Learned</h1>
-  </div>
+    <Header showForm={showForm} setShowForm={setShowForm}/>
+    
+    {showForm ? <NewFactForm/> : null}
 
-  <button class="btn btn-large btn-open">Share a fact</button>
-</header>
-<NewFact/>
+<main className="main">
 <CategoryFilter/>
+<FactList/>
+</main>
 </>
   );
 }
 
-function NewFact(){
-  return <form>Fact Form</form>;
+function Header({showForm,setShowForm}){
+  
+  const appTitle = "Today I learned";
+  return (
+    <header className="header">
+      <div className="logo">
+      <img src="logo512.png" height="68" width="68"alt="Today I Learned Logo"/>
+    <h1>{appTitle}</h1>
+  </div>
+  <button className="btn btn-large btn-open"
+  onClick={()=>setShowForm((show)=>!show)}
+  >{showForm?'close':"Share a fact"}</button>
+</header>
+
+  ) 
+}
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+function NewFactForm(){
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength =text.length;
+function handleSubmit(e){
+  e.preventDefault();
+}
+
+  return( <form className = "fact-form"onSubmit={handleSubmit}>
+  <input type="text" placeholder="Share a fact with the world..."value={text}
+  onChange={(e)=>setText(e.target.value)}
+  />
+  <span>{200-textLength}</span>
+  <input type="text" placeholder="Trustworthy source..."
+  onChange={(e)=>setSource(e.target.value)} />
+  <select value={category}onChange={(e)=>setCategory(e.target.value)}>
+    <option value="">Choose category:</option>
+    {CATEGORIES.map((cat) =>
+    (<option key={cat.name} value={cat.name}>{cat.name.toUpperCase()}
+    </option>))}
+  </select>
+  <button class="btn btn-large">Post</button>
+</form>);
 }
 
 function CategoryFilter(){
-  return <aside>Category Filter</aside>;
+  return (
+  <aside>
+    <ul>
+      <li className="category">
+        <button className="btn btn-all-categories">All</button>
+      </li>
+     {CATEGORIES.map((cat)=><li key ={cat.name} className="category">
+    <button
+      className="btn btn-category"
+      style={{backgroundColor: cat.color}}
+    >
+      {cat.name}
+    </button>
+  </li>)}
+
+  </ul>
+  </aside>
+);
 }
 
 function FactList(){
-  return <section>FactList</section>;
-
+  const facts= initialFacts;
+  return (
+  <section>
+  <ul className="facts-list">
+    {facts.map((fact) => (
+    <Fact key={fact.id} fact = {fact} /> 
+    ))}
+  </ul>
+  <p>There are {facts.length} facts in the database. Add your own!</p>
+  </section>
+  );
 }
+
+function Fact({ fact }){
+  return(
+  <li className="fact">
+  <p>
+    {fact.text}
+    <a className="source" href={fact.source}
+    target="_blank">
+      {fact.source}
+    </a>
+  </p>
+  <span className="tag" style={{backgroundColor: CATEGORIES.find((cat)=>cat.name ===fact.category).color}}>
+  {Fact.category}
+  </span>
+  <div className="vote-buttons">
+    <button>👍 {Fact.votesInteresting}</button>
+    <button>🤯 {Fact.votesMindblowing}</button>
+    <button>⛔️ {Fact.votesFalse}</button>
+  </div>
+</li>
+)}
 export default App;
